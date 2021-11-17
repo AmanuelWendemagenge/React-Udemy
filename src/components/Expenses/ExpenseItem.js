f702b660e5f67eb1957cfc2fe4c6c9ecef377539
amanuelgezahange12 <69604107+amanuelgezahange12@users.noreply.github.com>
2021-11-17 15:58:39 -0600

    commited

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,6 +3,7 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import React, {useState} from 'react' 
 function ExpenseItem(props) {
+  console.log(props)
 
   const [title,setTitle]=useState(props.title)
   const clickHandler=()=>{
@@ -13,12 +14,12 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
-  );
+  )
 }
 
 export default ExpenseItem;
